3d4563d8936aaa47fe79750e207cc1c48ebb0828
apoorva104 <srivastavaapoorva104@gmail.com>
2023-07-09 11:26:08 +0530

    Setup of sign-up form

--- index.js
+++ index.js
@@ -1,7 +1,11 @@
 const express = require('express')
 const app = express();
 const port = 8000;
+const cookieParser=require('cookie-parser');
 var expressLayouts = require('express-ejs-layouts');
+const db=require('./config/mongoose')
+app.use(express.urlencoded());
+app.use(cookieParser());
 app.use(expressLayouts);
 app.use(express.static('./assets'));
 app.set('layout extractStyles', true);
--- routes/users.js
+++ routes/users.js
@@ -2,6 +2,10 @@ const express=require('express');
 const router=express.Router();
 const userController=require('../controlers/users_controller');
 router.get('/profile',userController.profile);
+router.get('/sign-in',userController.sign_in);
+router.get('/sign-up',userController.sign_up);
+
+router.post('/create', userController.create);
 
 
 module.exports=router;
